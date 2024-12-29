f93d7bcb4deaea1f063068e0dd7be4a5481508b8
Winnysan <maarvinko@gmail.com>
2024-12-29 21:26:14 +0100

    log error to file

---
+++ src/lib/Logger.ts
@@ -0,0 +1,28 @@
+import fs from 'fs'
+import Message from './Message'
+
+/**
+ * A class resposible for logging message to a file
+ */
+class Logger {
+  /**
+   * Logs a message to file
+   * @param message
+   * @returns {void}
+   */
+  public logToFile(message: unknown): void {
+    try {
+      const errorMessage = Message.getErrorMessage(message)
+      const day = Helper.getCurrentDate()
+      const time = Helper.getCurrentTime()
+
+      const data = `${time} | ${errorMessage}`
+
+      fs.writeFileSync(`logs/${day}.txt`, data, { flag: 'a' })
+    } catch (err: unknown) {
+      console.error('logToFile', Message.getErrorMessage(err))
+    }
+  }
+}
+
+export default new Logger()
